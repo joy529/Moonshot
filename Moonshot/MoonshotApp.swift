c926bd1f8d0270c063d6e103e1431d156c7f3bf8
joy jain <joyshah204@gmial.com>
2023-12-09 00:41:13 +0530

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by JOY JAIN on 09/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
